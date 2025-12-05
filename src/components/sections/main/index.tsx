f8d7437e5e9acb72d436d71736c748f272f7104e
Emilm76 <uprwebdev@gmail.com>
2025-12-05 13:53:28 +0300

    add icons to main section

--- src/components/sections/main/index.tsx
+++ src/components/sections/main/index.tsx
@@ -1,7 +1,12 @@
 import clsx from 'clsx'
 import styles from './index.module.scss'
-import { LogoIcon } from '@/icons/logo'
 import { Carousel, CarouselLoop } from './carousel'
+import { LogoIcon } from '@/icons/logo'
+import CapIcon from '@/images/cap.png'
+import HumanIcon from '@/images/smart-human.png'
+import MouseIcon from '@/images/mouse.png'
+import FolderIcon from '@/images/folder.png'
+import Image from 'next/image'
 
 export function MainSection() {
   return (
@@ -13,7 +18,8 @@ export function MainSection() {
 
         <div className={styles.container}>
           <h1 className={clsx(styles.title, 'h1')}>
-            Расти. Достигай. Побеждай вместе со&nbsp;Стройтех и&nbsp;ЮгСтройИнвест
+            Расти. Достигай. Побеждай вместе со&nbsp;Стройтех
+            и&nbsp;ЮгСтройИнвест
           </h1>
 
           <Carousel
@@ -21,22 +27,25 @@ export function MainSection() {
               <div className={clsx(styles.slide, 'embla__slide')} key={1}>
                 <div className="h3">100+</div>
                 <p>
-                  Сотрудников прошли обучение и уже показывают реальные результаты: уверенные продажи, сильная
-                  коммуникация и высокий уровень профессионализма в работе.
+                  Сотрудников прошли обучение и уже показывают реальные
+                  результаты: уверенные продажи, сильная коммуникация и высокий
+                  уровень профессионализма в работе.
                 </p>
               </div>,
               <div className={clsx(styles.slide, 'embla__slide')} key={2}>
                 <div className="h3">50+</div>
                 <p>
-                  Практических курсов, методических записок, тестирований созданных внутренними экспертами
-                  ЮгСтройИнвест.
+                  Практических курсов, методических записок, тестирований
+                  созданных внутренними экспертами ЮгСтройИнвест.
                 </p>
               </div>,
               <div className={clsx(styles.slide, 'embla__slide')} key={3}>
                 <div className="h3">5+</div>
                 <p>
-                  Стройтех объединяет филиалы компании по всей стране - от Ставрополя до Мариуполя. Обучение доступно
-                  каждому сотруднику, где бы он ни находился, единые стандарты, общая философия, общая цель.
+                  Стройтех объединяет филиалы компании по всей стране - от
+                  Ставрополя до Мариуполя. Обучение доступно каждому сотруднику,
+                  где бы он ни находился, единые стандарты, общая философия,
+                  общая цель.
                 </p>
               </div>,
             ]}
@@ -60,6 +69,19 @@ export function MainSection() {
             'Следи за прогрессом',
           ]}
         />
+
+        <div className={styles.capIcon}>
+          <Image src={CapIcon} alt="" />
+        </div>
+        <div className={styles.humanIcon}>
+          <Image src={HumanIcon} alt="" />
+        </div>
+        <div className={styles.mouseIcon}>
+          <Image src={MouseIcon} alt="" />
+        </div>
+        <div className={styles.folderIcon}>
+          <Image src={FolderIcon} alt="" />
+        </div>
       </div>
     </section>
   )
